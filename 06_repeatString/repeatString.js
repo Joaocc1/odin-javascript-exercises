567176c24284fe60a42d035a051048dbd45aa764
Joaoc <joaoc86@gmail.com>
2025-11-30 21:34:27 +0000

    solve exercises 06 to 08

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  let newStr = "";
 
+  if (num >= 0) {
+    for (let i = 1; i <= num; i++) {
+      newStr += str;
+    }
+  } else {
+    newStr = "ERROR";
+  }
+
+  return newStr;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...items) {
+  if (!items) {
+    return arr;
+  }
+
+  for (const item of items) {
+    if (arr.includes(item)) {
+      for (const arrItem of arr) {
+        if (arrItem === item) {
+          arr.splice(arr.indexOf(item), 1, undefined);
+        }
+      }
+    }
+  }
+
+  return arr.filter((filteredItem) => filteredItem !== undefined);
 };
+
+// console.log(removeFromArray([1, 2, 3, 3, 4, 4, 4, 4], 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
