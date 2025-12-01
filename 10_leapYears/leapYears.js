8073a45b9b9f3668e8f3e1b367c2f9fe07ab74f2
Joaoc <joaoc86@gmail.com>
2025-12-01 15:54:00 +0000

    solve exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (!Number.isInteger(year)) return "ERROR";
 
+  if (year % 100 === 0) {
+    if (year % 400 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+// console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
